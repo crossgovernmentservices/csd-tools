fd111d3430655b9173bd764a299257f3a040cfcd
Kerin Cosford <cosford@gmail.com>
2016-04-11 18:26:11 +0100

    Added IAM profile to Jenkins instance

--- terraform/aws-instances.tf
+++ terraform/aws-instances.tf
@@ -6,6 +6,8 @@ resource "aws_instance" "jenkins" {
   subnet_id = "${aws_subnet.public.id}"
   vpc_security_group_ids = ["${aws_security_group.jenkins.id}"]
   associate_public_ip_address = true
+  iam_instance_profile = "${aws_iam_instance_profile.jenkins_profile.id}"
+
 
   tags {
     Name = "csd-tools-jenkins"
