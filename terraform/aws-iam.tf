ec4439fbc16df31bea5f5f0e5b34397355a570a4
Kerin Cosford <cosford@gmail.com>
2016-04-11 18:25:58 +0100

    Added Jenkins IAM policy

---
+++ terraform/aws-iam.tf
@@ -0,0 +1,93 @@
+resource "aws_iam_role" "jenkins_role" {
+    name = "jenkins_role"
+    path = "/"
+    assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {"AWS": "*"},
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "jenkins_profile" {
+    name = "jenkins_profile"
+    roles = ["${aws_iam_role.jenkins_role.name}"]
+}
+
+resource "aws_iam_policy" "terraform_policy" {
+    name = "terraform_policy"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:*"
+      ],
+      "Resource": [
+        "*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "rds:*"
+      ],
+      "Resource": [
+        "*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:*"
+      ],
+      "Resource": [
+        "*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "iam:*"
+      ],
+      "Resource": [
+        "*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "elasticloadbalancing:*"
+      ],
+      "Resource": [
+        "*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "autoscaling:*"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "jenkins_terraform_attachment" {
+    name = "jenkins_terraform_attachment"
+    roles = ["${aws_iam_role.jenkins_role.name}"]
+    policy_arn = "${aws_iam_policy.terraform_policy.arn}"
+}
