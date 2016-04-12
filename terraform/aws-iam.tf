cf362b9eb75481469526a20306f568697e0decab
Kerin Cosford <cosford@gmail.com>
2016-04-12 14:25:04 +0100

    Granted Jenkins role access to S3

--- terraform/aws-iam.tf
+++ terraform/aws-iam.tf
@@ -80,6 +80,15 @@ resource "aws_iam_policy" "terraform_policy" {
       "Resource": [
         "*"
       ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "*"
+      ]
     }
   ]
 }
